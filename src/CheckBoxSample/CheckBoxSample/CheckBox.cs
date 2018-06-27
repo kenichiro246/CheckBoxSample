f5205f07878e61ea8c7214b0b5fa1c78ecacf308
kenichiro246 <tomorrow.is.another.day.246@gmail.com>
2018-06-27 19:20:16 +0900

    add solution files

---
+++ src/CheckBoxSample/CheckBoxSample/CheckBox.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+
+namespace CheckBoxSample
+{
+    public class CheckBox : Button
+    {
+        public CheckBox()
+        {
+            this.Clicked += ((s, e) =>
+              {
+                  IsCheck = !IsCheck;
+              });
+            Image = getImageFileName(false);
+            this.BackgroundColor = Color.White;
+        }
+
+        public static BindableProperty IsCheckProperty = 
+            BindableProperty.Create(nameof(IsCheck),typeof(bool), typeof(CheckBox), false,
+                propertyChanged: CheckedValueChanged);
+
+        public bool IsCheck
+        {
+            get { return (bool)GetValue(IsCheckProperty); }
+            set { SetValue(IsCheckProperty, value); }
+        }
+
+        private static void CheckedValueChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            (bindable as Button).Image = getImageFileName((bool)newValue);
+        }
+
+        private static string getImageFileName(bool value)
+        {
+            return value ? "icon_check_b96.png" : "icon_check_empty_b96.png";
+        }
+    }
+}
